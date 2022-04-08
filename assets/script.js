2590d83bef3545a4e0914eae35745c91b18ed0a2
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-04-07 17:24:18 -0700

    Loop through each time block and add class based on comparison between currentHour and blockHour which were booth parsed to whole integers

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,53 @@
 var currentDate = moment().format("MMMM Do YYYY"); 
 $('#currentDay').append(currentDate);
 
-// create a time block div for each hour until 5pm.
-//containing a child div displaying the hour, a text area for user input, and a save button
+// add a time block div for each hour until 5pm.
+
+// on click on save btn, a function sets the value of it most recent sibling, the text area el, in local storage with the key as the id (#hour-9..) of its parent div
 
 //when the page loads, retrieve the existing array of time block schedule items and display them on the page 
 
-// when the user clicks on a time block div and the event target has a class = 'btn', a function should be called that sets the value of the div's text area to the array of schedule items
+// console.log(scheduleItem)
+var saveBtn = $('.saveBtn')
+var blockInput = (saveBtn.prev().val())
+var blockId = (saveBtn.parent().attr('id'))
 
-// the array of schedule items should be set in local storage
+blockInput.append = localStorage.getItem(blockId)
 
-
-var currentTime = moment().format("hA")
-
-//FOR each div with class = hour
-// IF the div's text content < currentTime, give it's parent div class = 'past'
-// Else if if the div's text content == current time, give it parent div class = 'present'
-// Else give it's parent div a class == 'future'
-var hourDivs = $('.hour')
-for (var i = 0; i < hourDivs.length; i ++){
-    console.log(hourDivs[i].textContent)
+for (var i = 0; i < localStorage.length; i ++){
+    console.log(window.localStorage.getItem('blockId'[i]))
 }
+    
+saveBtn.on('click', function () {
+    // console.log(scheduleItem.val()) // this works?
+    blockInput = ($(this).prev().val())
+    console.log(blockInput)
+    blockId = ($(this).parent().attr('id'))
+    console.log(blockId)
+    localStorage.setItem(blockId, blockInput)
+    
+})
 
+
+// // the array of schedule items should be set in local storage
+
+var currentTime =  parseInt(moment().format("H"))
+console.log(currentTime)
+
+// //FOR each div with class = time-block
+// // IF the div's id split at '-' and parsed to integer < currentTime parsed to integer, add class 'past' 
+// // Else if if the div's hour id == current time, add class 'present' its parent div
+// // Else add class 'future' to  it's parent div
+
+var timeBlockEl = $('.time-block')
+
+for (var i = 0; i < timeBlockEl.length; i++) {
+        var blockTime = parseInt(timeBlockEl[i].id.split("-")[1])
+// if blockTime < currentTime, add class past to timeBlockEl
+    if (blockTime < currentTime){
+        timeBlockEl.eq(i).addClass('past') //eq(i) annoying jQuery syntax for index
+    } else if (blockTime > currentTime) {
+        timeBlockEl.eq(i).addClass('future')
+    } else timeBlockEl.eq(i).addClass('present')
+}
 
